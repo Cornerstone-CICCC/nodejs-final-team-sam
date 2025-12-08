c46f43d35a249fa937526a4c59630f91359df5cf
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-12-07 16:15:35 -0800

    Removing the function added by the lates merge, no need to merge with main

--- frontend/src/components/ChatHead.tsx
+++ frontend/src/components/ChatHead.tsx
@@ -28,7 +28,6 @@ const ChatHead = ({roomId, showMember, onTrigger}:ChatHeadProps) => {
     //leave room handler
     const leaveRoomHandler=()=>{
         leaveRoom()
-        if(onTrigger) onTrigger
         //send leave room via socket.io
         navigate('/chats')
     }
